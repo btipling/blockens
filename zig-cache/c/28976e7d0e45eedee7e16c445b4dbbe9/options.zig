ff2494e3d65cb4da27b4d5cd948fee4c9770353f
btipling <bjorn@ambientchill.com>
2023-12-03 10:56:44 -0800

    remove zig-cache

--- zig-cache/c/28976e7d0e45eedee7e16c445b4dbbe9/options.zig
+++
@@ -1,8 +0,0 @@
-pub const @"libs.zig-gamedev.libs.zgui.build.Backend" = enum {
-    no_backend,
-    glfw_wgpu,
-    glfw_opengl3,
-    win32_dx12,
-};
-pub const backend: @"libs.zig-gamedev.libs.zgui.build.Backend" = .glfw_opengl3;
-pub const shared: bool = false;
--- zig-cache/c/c11cada2c1c7212e4e1f16a1c3b00f46/options.zig
+++
@@ -1,5 +0,0 @@
-pub const @"libs.zig-gamedev.libs.zopengl.build.API" = enum {
-    raw_bindings,
-    wrapper,
-};
-pub const api: @"libs.zig-gamedev.libs.zopengl.build.API" = .raw_bindings;
