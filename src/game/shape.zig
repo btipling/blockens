4d0b18033c5858ff93de789e3537fc71f04a7060
btipling <bjorn@ambientchill.com>
2023-12-17 13:59:03 -0800

    turn off texture filtering

--- src/game/shape.zig
+++ src/game/shape.zig
@@ -218,8 +218,8 @@ pub const Shape = struct {
 
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         e = gl.getError();
         if (e != gl.NO_ERROR) {
             std.debug.print("{s} text parameter i error: {d}\n", .{ msg, e });
