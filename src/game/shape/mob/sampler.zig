c41dd39a20a5ad2a981c31177e56383fea94d492
btipling <bjorn@ambientchill.com>
2024-02-10 12:57:47 -0800

    Reading frames :O

--- src/game/shape/mob/sampler.zig
+++ src/game/shape/mob/sampler.zig
@@ -4,6 +4,10 @@ const gl = @import("zopengl");
 const zm = @import("zmath");
 const gltf = zmesh.io.zcgltf;
 
+pub const SamplerErr = error{
+    MissingDataErr,
+};
+
 pub const Sampler = struct {
     alloc: std.mem.Allocator,
     node: *gltf.Node,
@@ -34,8 +38,7 @@ pub const Sampler = struct {
 
     pub fn build(self: *Sampler) !void {
         const nodeName = self.node.name orelse "no node name";
-        const input = self.sampler.input;
-        buildAnimationFromTime(input);
+        try self.buildFrames();
         switch (self.targetPath) {
             .translation => {
                 std.debug.print("found translation animation for node {s} in {s}\n", .{ nodeName, self.name });
@@ -60,20 +63,51 @@ pub const Sampler = struct {
         }
     }
 
-    pub fn buildAnimationFromTime(acessor: *gltf.Accessor) void {
+    fn buildFrames(self: *Sampler) !void {
+        const framesData = self.sampler.input;
+        self.numFrames = framesData.count;
         std.debug.print("input time has {d} elements, and {d} byte offset with stride of, {d} ", .{
-            acessor.count,
-            acessor.offset,
-            acessor.stride,
+            framesData.count,
+            framesData.offset,
+            framesData.stride,
         });
-        switch (acessor.component_type) {
+        std.debug.print(" is normalized {d}, ", .{framesData.normalized});
+        switch (framesData.component_type) {
             .r_32f => std.debug.print("it has r_32f component type ", .{}),
             else => std.debug.print("it has invalid component type for input time ", .{}),
         }
 
-        switch (acessor.type) {
+        switch (framesData.type) {
             .scalar => std.debug.print("and scalar type\n", .{}),
             else => std.debug.print("invalid time input", .{}),
+        }
+
+        if (framesData.buffer_view == null) {
+            return SamplerErr.MissingDataErr;
+        }
+
+        try self.readFramesBuffer(framesData.buffer_view.?, framesData.offset);
+    }
+
+    fn printBufferViewType(bufferView: *gltf.BufferView) void {
+        switch (bufferView.view_type) {
+            .invalid => std.debug.print("invalid buffer view type\n", .{}),
+            .indices => std.debug.print("buffer view type is indicies\n", .{}),
+            .vertices => std.debug.print("buffer view type is vertices\n", .{}),
+        }
+    }
+
+    fn readFramesBuffer(self: *Sampler, bufferView: *gltf.BufferView, accessorOffset: usize) !void {
+        printBufferViewType(bufferView);
+        const bufferData = bufferView.buffer.data orelse {
+            return SamplerErr.MissingDataErr;
+        };
+        std.debug.print("buffer view offset: {d}\n", .{bufferView.offset});
+
+        const dataAddr = @as([*]const u8, @ptrCast(bufferData)) + accessorOffset + bufferView.offset;
+        const framesData = @as([*]const f32, @ptrCast(@alignCast(dataAddr)));
+        for (0..self.numFrames) |i| {
+            std.debug.print("found frame with {d} value\n", .{framesData[i]});
         }
     }
 
