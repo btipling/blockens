d57a114632e754f2904d87a3839ed53852836945
btipling <bjorn@ambientchill.com>
2024-02-18 08:47:57 -0800

    Organize entity initialization

--- src/game/blecs/entities/entities.zig
+++ src/game/blecs/entities/entities.zig
@@ -1,67 +1,7 @@
-const std = @import("std");
-const ecs = @import("zflecs");
-const gl = @import("zopengl");
-const math = @import("../../math/math.zig");
-const game = @import("../../game.zig");
-const components = @import("../components/components.zig");
-const helpers = @import("../helpers.zig");
-const tags = @import("../tags.zig");
+const gfx = @import("entities_gfx.zig");
+const screen = @import("entities_screen.zig");
 
 pub fn init() void {
-    game.state.entities.screen = ecs.new_entity(game.state.world, "Screen");
-    const gameData = ecs.new_entity(game.state.world, "ScreenGameData");
-    const settingsData = ecs.new_entity(game.state.world, "ScreenSettingsData");
-    const initialScreen = helpers.new_child(game.state.world, game.state.entities.screen);
-    _ = ecs.add(game.state.world, initialScreen, components.screen.Game);
-    _ = ecs.set(game.state.world, game.state.entities.screen, components.screen.Screen, .{
-        .current = initialScreen,
-        .gameDataEntity = gameData,
-        .settingDataEntity = settingsData,
-    });
-
-    game.state.entities.clock = ecs.new_entity(game.state.world, "Clock");
-    _ = ecs.set(game.state.world, game.state.entities.clock, components.Time, .{ .startTime = 0, .currentTime = 0 });
-
-    game.state.entities.gfx = ecs.new_entity(game.state.world, "Gfx");
-    _ = ecs.set(game.state.world, game.state.entities.gfx, components.gfx.BaseRenderer, .{
-        .clear = gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT,
-        .bgColor = math.vecs.Vflx4.initBytes(135, 206, 235, 255),
-    });
-
-    game.state.entities.sky = ecs.new_entity(game.state.world, "Sky");
-    _ = ecs.set(game.state.world, game.state.entities.sky, components.Sky, .{
-        .sun = .rising,
-    });
-
-    game.state.entities.crosshair = ecs.new_entity(game.state.world, "Crosshair");
-    const c_hrz = helpers.new_child(game.state.world, game.state.entities.crosshair);
-    _ = ecs.add(game.state.world, c_hrz, components.shape.Shape);
-    const cr_c = math.vecs.Vflx4.initBytes(33, 33, 33, 255);
-    _ = ecs.set(
-        game.state.world,
-        c_hrz,
-        components.shape.Color,
-        components.shape.Color.fromVec(cr_c),
-    );
-    _ = ecs.set(game.state.world, c_hrz, components.shape.Scale, .{ .x = 0.025, .y = 0.004, .z = 1 });
-    _ = ecs.set(game.state.world, c_hrz, components.shape.Translation, .{ .x = -0.5, .y = -0.5, .z = 0 });
-    _ = ecs.add(game.state.world, c_hrz, components.shape.NeedsSetup);
-    ecs.add_pair(game.state.world, c_hrz, ecs.ChildOf, gameData);
-    const c_vrt = helpers.new_child(game.state.world, game.state.entities.crosshair);
-    _ = ecs.add(game.state.world, c_vrt, components.shape.Shape);
-    _ = ecs.set(
-        game.state.world,
-        c_vrt,
-        components.shape.Color,
-        components.shape.Color.fromVec(cr_c),
-    );
-    _ = ecs.set(game.state.world, c_vrt, components.shape.Scale, .{ .x = 0.0025, .y = 0.04, .z = 1 });
-    _ = ecs.set(game.state.world, c_vrt, components.shape.Translation, .{ .x = -0.5, .y = -0.5, .z = 0 });
-    _ = ecs.add(game.state.world, c_vrt, components.shape.NeedsSetup);
-    ecs.add_pair(game.state.world, c_vrt, ecs.ChildOf, gameData);
-
-    game.state.entities.menu = ecs.new_entity(game.state.world, "Menu");
-    _ = ecs.set(game.state.world, game.state.entities.menu, components.ui.Menu, .{
-        .visible = false,
-    });
+    gfx.init();
+    screen.init();
 }
---
+++ src/game/blecs/entities/entities_gfx.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const ecs = @import("zflecs");
+const gl = @import("zopengl");
+const math = @import("../../math/math.zig");
+const game = @import("../../game.zig");
+const components = @import("../components/components.zig");
+const helpers = @import("../helpers.zig");
+const tags = @import("../tags.zig");
+
+pub fn init() void {
+    initBaseRenderer();
+    initClock();
+    initSky();
+}
+
+fn initBaseRenderer() void {
+    game.state.entities.gfx = ecs.new_entity(game.state.world, "Gfx");
+    _ = ecs.set(game.state.world, game.state.entities.gfx, components.gfx.BaseRenderer, .{
+        .clear = gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT,
+        .bgColor = math.vecs.Vflx4.initBytes(135, 206, 235, 255),
+    });
+}
+
+fn initClock() void {
+    game.state.entities.clock = ecs.new_entity(game.state.world, "Clock");
+    _ = ecs.set(game.state.world, game.state.entities.clock, components.Time, .{ .startTime = 0, .currentTime = 0 });
+}
+
+fn initSky() void {
+    game.state.entities.sky = ecs.new_entity(game.state.world, "Sky");
+    _ = ecs.set(game.state.world, game.state.entities.sky, components.Sky, .{
+        .sun = .rising,
+    });
+}
