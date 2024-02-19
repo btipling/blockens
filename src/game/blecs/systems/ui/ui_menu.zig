f0c51a04e4f66a274af0cdd270129ec0b26c0afe
btipling <bjorn@ambientchill.com>
2024-02-19 14:58:23 -0800

    Stub out better menu

--- src/game/blecs/systems/ui/ui_menu.zig
+++ src/game/blecs/systems/ui/ui_menu.zig
@@ -31,15 +31,36 @@ fn run(it: *ecs.iter_t) callconv(.C) void {
             game.state.window.setInputMode(glfw.InputMode.cursor, glfw.Cursor.Mode.normal);
             if (zgui.beginMainMenuBar()) {
                 zgui.pushStyleVar2f(.{ .idx = .item_spacing, .v = [2]f32{ 20.0, 20.0 } });
-                if (zgui.menuItem("game", .{})) {
-                    screen_helpers.showGameScreen();
+                if (zgui.beginMenu("Game", true)) {
+                    if (zgui.menuItem("Play", .{})) {
+                        screen_helpers.showGameScreen();
+                    }
+                    if (zgui.menuItem("Exit", .{})) {
+                        game.state.quit = true;
+                    }
+                    zgui.endMenu();
                 }
-                if (zgui.menuItem("settings", .{})) {
-                    screen_helpers.showSettingsScreen();
+                if (zgui.beginMenu("Build Tools", true)) {
+                    if (zgui.menuItem("World", .{})) {
+                        screen_helpers.showSettingsScreen();
+                    }
+                    if (zgui.menuItem("Block Textures", .{})) {
+                        screen_helpers.showSettingsScreen();
+                    }
+                    if (zgui.menuItem("Block", .{})) {
+                        screen_helpers.showSettingsScreen();
+                    }
+                    if (zgui.menuItem("Chunk", .{})) {
+                        screen_helpers.showSettingsScreen();
+                    }
+                    if (zgui.menuItem("Character", .{})) {
+                        screen_helpers.showSettingsScreen();
+                    }
+                    zgui.endMenu();
                 }
                 const ww = zgui.getWindowWidth();
-                zgui.sameLine(.{ .offset_from_start_x = ww - 150.0 });
-                if (zgui.menuItem("exit", .{})) {
+                zgui.sameLine(.{ .offset_from_start_x = ww - 50.0 });
+                if (zgui.menuItem("X", .{})) {
                     game.state.quit = true;
                 }
                 zgui.popStyleVar(.{ .count = 1 });
