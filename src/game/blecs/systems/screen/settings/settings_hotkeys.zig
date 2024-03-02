c27282d7dd247c7ee23d24bf52d114cd4bd55039
btipling <bjorn@ambientchill.com>
2024-03-02 03:36:43 -0800

    key commands

--- src/game/blecs/systems/screen/settings/settings_hotkeys.zig
+++ src/game/blecs/systems/screen/settings/settings_hotkeys.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const ecs = @import("zflecs");
+const zm = @import("zmath");
 const gl = @import("zopengl").bindings;
 const components = @import("../../../components/components.zig");
 const game = @import("../../../../game.zig");
@@ -18,11 +19,40 @@ fn system() ecs.system_desc_t {
 }
 
 fn run(it: *ecs.iter_t) callconv(.C) void {
+    const world = it.world;
+    const screen: *const components.screen.Screen = ecs.get(
+        world,
+        game.state.entities.screen,
+        components.screen.Screen,
+    ) orelse unreachable;
     while (ecs.iter_next(it)) {
         for (0..it.count()) |_| {
-            if (input.keys.pressedKey(.F3)) {
-                std.debug.print("F3 pressed on settings screen!\n", .{});
+            if (ecs.has_id(world, screen.current, ecs.id(components.screen.ChunkEditor))) {
+                handleChunkHotKeys();
             }
         }
+    }
+}
+
+fn handleChunkHotKeys() void {
+    if (input.keys.holdKey(.left)) {
+        var world_rotation: *components.screen.WorldRotation = ecs.get_mut(
+            game.state.world,
+            game.state.entities.settings_camera,
+            components.screen.WorldRotation,
+        ) orelse return;
+        const m = zm.quatToMat(world_rotation.rotation);
+        const r = zm.rotationY(0.0125 * std.math.pi * 2.0);
+        world_rotation.rotation = zm.matToQuat(zm.mul(m, r));
+    }
+    if (input.keys.holdKey(.right)) {
+        var world_rotation: *components.screen.WorldRotation = ecs.get_mut(
+            game.state.world,
+            game.state.entities.settings_camera,
+            components.screen.WorldRotation,
+        ) orelse return;
+        const m = zm.quatToMat(world_rotation.rotation);
+        const r = zm.rotationY(-0.0125 * std.math.pi * 2.0);
+        world_rotation.rotation = zm.matToQuat(zm.mul(m, r));
     }
 }
