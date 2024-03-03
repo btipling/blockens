9df874c8c741b34f516a89c90f05554787202071
btipling <bjorn@ambientchill.com>
2024-03-03 01:35:11 -0800

    World loaded.

--- src/game/blecs/systems/block/block_rendering.zig
+++ src/game/blecs/systems/block/block_rendering.zig
@@ -87,14 +87,20 @@ fn render_mesh(world: *ecs.world_t, entity: ecs.entity_t, loc: @Vector(4, gl.Flo
         var block_id: u8 = 0;
         block_id = @intCast(c.data[i]);
         if (block_id == 0) continue; // Some kind of one off bug somewhere?
-        if (!game.state.gfx.settings_blocks.contains(block_id)) {
+        var blocks_map: *std.AutoHashMap(u8, *game_state.BlockInstance) = undefined;
+        if (parent == entities.screen.game_data) {
+            blocks_map = &game.state.gfx.game_blocks;
+        } else {
+            blocks_map = &game.state.gfx.settings_blocks;
+        }
+        if (!blocks_map.contains(block_id)) {
             const block_entity = helpers.new_child(world, parent);
             const bi: *game_state.BlockInstance = game.state.allocator.create(game_state.BlockInstance) catch unreachable;
             bi.* = .{
                 .entity_id = block_entity,
                 .transforms = std.ArrayList(zm.Mat).init(game.state.allocator),
             };
-            game.state.gfx.settings_blocks.put(block_id, bi) catch unreachable;
+            blocks_map.put(block_id, bi) catch unreachable;
             ecs.add(world, bi.entity_id, components.block.Instance);
             _ = ecs.set(world, bi.entity_id, components.shape.Shape, .{ .shape_type = .cube });
             const cr_c = math.vecs.Vflx4.initBytes(0, 0, 0, 0);
@@ -110,7 +116,7 @@ fn render_mesh(world: *ecs.world_t, entity: ecs.entity_t, loc: @Vector(4, gl.Flo
             // ecs.add(game.state.world, bi.entity_id, components.Debug);
             ecs.add(world, bi.entity_id, components.shape.NeedsSetup);
         }
-        const bi: *game_state.BlockInstance = game.state.gfx.settings_blocks.get(block_id).?;
+        const bi: *game_state.BlockInstance = blocks_map.get(block_id).?;
         ecs.add(world, bi.entity_id, components.gfx.NeedsInstanceDataUpdate);
         const p: @Vector(4, gl.Float) = chunk.getPositionAtIndexV(i);
         const fp: @Vector(4, gl.Float) = .{ p[0] + loc[0], p[1] + loc[1], p[2] + loc[2], p[3] + loc[3] };
