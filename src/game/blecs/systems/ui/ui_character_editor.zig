09f82e53e05eaedffa871808f29a8d066d5692b6
btipling <bjorn@ambientchill.com>
2024-03-03 22:15:46 -0800

    I think that's for UIs to transition

--- src/game/blecs/systems/ui/ui_character_editor.zig
+++ src/game/blecs/systems/ui/ui_character_editor.zig
@@ -24,19 +24,60 @@ fn system() ecs.system_desc_t {
 fn run(it: *ecs.iter_t) callconv(.C) void {
     while (ecs.iter_next(it)) {
         for (0..it.count()) |_| {
-            const xPos: f32 = 700.0;
+            const xPos: f32 = 2200.0;
             const yPos: f32 = 50.0;
             zgui.setNextWindowPos(.{ .x = xPos, .y = yPos, .cond = .always });
             zgui.setNextWindowSize(.{
-                .w = 2850,
+                .w = 1600,
                 .h = 2000,
             });
-            if (zgui.begin("Character Editor", .{
-                .flags = .{},
+            zgui.setNextItemWidth(-1);
+            if (zgui.begin("Character Designer", .{
+                .flags = .{
+                    .no_title_bar = false,
+                    .no_resize = false,
+                    .no_scrollbar = false,
+                    .no_collapse = false,
+                },
             })) {
-                zgui.text("TODO: character editor", .{});
+                drawControls() catch unreachable;
             }
             zgui.end();
         }
     }
+}
+
+fn drawControls() !void {
+    if (zgui.beginChild(
+        "Saved Worlds",
+        .{
+            .w = 510,
+            .h = 2100,
+            .border = true,
+        },
+    )) {
+        zgui.pushStyleVar2f(.{ .idx = .frame_padding, .v = [2]f32{ 10.0, 10.0 } });
+        if (zgui.button("Generate character", .{
+            .w = 500,
+            .h = 100,
+        })) {
+            try generateCharacter();
+        }
+        if (zgui.button("Toggle walking", .{
+            .w = 500,
+            .h = 100,
+        })) {
+            try toggleWalking();
+        }
+        zgui.popStyleVar(.{ .count = 1 });
+    }
+    zgui.endChild();
+}
+
+fn generateCharacter() !void {
+    // try appState.character.generate();
+}
+
+fn toggleWalking() !void {
+    // try appState.character.toggleWalking();
 }
