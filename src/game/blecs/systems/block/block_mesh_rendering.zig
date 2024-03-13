5dbabc83d93578fb89be65e1f5a97f86e0c6fbc2
btipling <bjorn@ambientchill.com>
2024-03-13 00:16:22 -0700

    So close to multidraw. Need texture atlas first
    will add to single draw meshes first

--- src/game/blecs/systems/block/block_mesh_rendering.zig
+++ src/game/blecs/systems/block/block_mesh_rendering.zig
@@ -91,54 +91,45 @@ fn render_multidraw(world: *ecs.world_t, entity: ecs.entity_t, loc: @Vector(4, f
         c = game.state.gfx.settings_chunks.get(wp) orelse return;
     }
 
-    var keys = c.meshes.keyIterator();
-    while (keys.next()) |_k| {
-        const i: usize = _k.*;
-        if (c.meshes.get(i)) |s| {
-            _ = s;
-            // const mb_e = helpers.new_child(world, parent);
-            //     var block_id: u8 = 0;
-            //     block_id = @intCast(c.data[i]);
-            //     if (block_id == 0) continue; // Some kind of one off bug somewhere?
-            //     var blocks_map: *std.AutoHashMap(u8, *game_state.BlockInstance) = undefined;
-            //     if (parent == entities.screen.game_data) {
-            //         blocks_map = &game.state.gfx.game_blocks;
-            //     } else {
-            //         blocks_map = &game.state.gfx.settings_blocks;
-            //     }
-            //     if (!blocks_map.contains(block_id)) {
-            //         const block_entity = helpers.new_child(world, parent);
-            //         const bi: *game_state.BlockInstance = game.state.allocator.create(game_state.BlockInstance) catch unreachable;
-            //         bi.* = .{
-            //             .entity_id = block_entity,
-            //             .transforms = std.ArrayList(zm.Mat).init(game.state.allocator),
-            //         };
-            //         blocks_map.put(block_id, bi) catch unreachable;
-            //         ecs.add(world, bi.entity_id, components.block.Instance);
-            //         _ = ecs.set(world, bi.entity_id, components.shape.Shape, .{ .shape_type = .cube });
-            //         const cr_c = math.vecs.Vflx4.initBytes(0, 0, 0, 0);
-            //         _ = ecs.set(world, bi.entity_id, components.shape.Color, components.shape.Color.fromVec(cr_c));
-            // if (parent == entities.screen.game_data) {
-            //     _ = ecs.set(world, bi.entity_id, components.shape.UBO, .{ .binding_point = gfx.constants.GameUBOBindingPoint });
-            // } else {
-            //     _ = ecs.set(world, bi.entity_id, components.shape.UBO, .{ .binding_point = gfx.constants.SettingsUBOBindingPoint });
-            // }
-            //         _ = ecs.set(world, bi.entity_id, components.block.Block, .{
-            //             .block_id = block_id,
-            //         });
-            //         // ecs.add(game.state.world, bi.entity_id, components.Debug);
-            //         ecs.add(world, bi.entity_id, components.shape.NeedsSetup);
-            //     }
-            //     const bi: *game_state.BlockInstance = blocks_map.get(block_id).?;
-            //     ecs.add(world, bi.entity_id, components.gfx.NeedsInstanceDataUpdate);
-            //     const p: @Vector(4, f32) = chunk.getPositionAtIndexV(i);
-            //     const fp: @Vector(4, f32) = .{ p[0] + 0.5 + loc[0], p[1] + 0.5 + loc[1], p[2] + 0.5 + loc[2], p[3] + loc[3] };
-            //     bi.transforms.append(zm.translationV(fp)) catch |e| {
-            //         std.debug.print("got an error appending transforms? {}\n", .{e});
-            //     };
-        }
+    for (c.elements.items, 0..) |element, i| {
+        var block_id: u8 = element.block_id;
+        block_id = @intCast(c.data[i]);
+        if (block_id == 0) continue; // Some kind of one off bug somewhere?
+        //     var blocks_map: *std.AutoHashMap(u8, *game_state.BlockInstance) = undefined;
+        //     if (parent == entities.screen.game_data) {
+        //         blocks_map = &game.state.gfx.game_blocks;
+        //     } else {
+        //         blocks_map = &game.state.gfx.settings_blocks;
+        //     }
+        //     if (!blocks_map.contains(block_id)) {
+        //         const block_entity = helpers.new_child(world, parent);
+        //         const bi: *game_state.BlockInstance = game.state.allocator.create(game_state.BlockInstance) catch unreachable;
+        //         bi.* = .{
+        //             .entity_id = block_entity,
+        //             .transforms = std.ArrayList(zm.Mat).init(game.state.allocator),
+        //         };
+        //         blocks_map.put(block_id, bi) catch unreachable;
+        //         ecs.add(world, bi.entity_id, components.block.Instance);
+        //         _ = ecs.set(world, bi.entity_id, components.shape.Shape, .{ .shape_type = .cube });
+        //         const cr_c = math.vecs.Vflx4.initBytes(0, 0, 0, 0);
+        //         _ = ecs.set(world, bi.entity_id, components.shape.Color, components.shape.Color.fromVec(cr_c));
+        // if (parent == entities.screen.game_data) {
+        //     _ = ecs.set(world, bi.entity_id, components.shape.UBO, .{ .binding_point = gfx.constants.GameUBOBindingPoint });
+        // } else {
+        //     _ = ecs.set(world, bi.entity_id, components.shape.UBO, .{ .binding_point = gfx.constants.SettingsUBOBindingPoint });
+        // }
+        //         _ = ecs.set(world, bi.entity_id, components.block.Block, .{
+        //             .block_id = block_id,
+        //         });
+        //         // ecs.add(game.state.world, bi.entity_id, components.Debug);
+        //         ecs.add(world, bi.entity_id, components.shape.NeedsSetup);
+        //     }
+        //     const bi: *game_state.BlockInstance = blocks_map.get(block_id).?;
+        //     ecs.add(world, bi.entity_id, components.gfx.NeedsInstanceDataUpdate);
+        //     const p: @Vector(4, f32) = chunk.getPositionAtIndexV(i);
+        //     const fp: @Vector(4, f32) = .{ p[0] + 0.5 + loc[0], p[1] + 0.5 + loc[1], p[2] + 0.5 + loc[2], p[3] + loc[3] };
+        //     bi.transforms.append(zm.translationV(fp)) catch |e| {
+        //         std.debug.print("got an error appending transforms? {}\n", .{e});
+        //     };
     }
-    // if (parent == entities.screen.game_data) game.state.allocator.free(c.data);
-    // c.deinit();
-    // game.state.allocator.destroy(c);
 }
