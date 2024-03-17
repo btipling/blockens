b526e43c721a007246992143f6d600dbf839f2fa
btipling <bjorn@ambientchill.com>
2024-03-16 17:25:52 -0700

    Toggle chunk visibility

--- src/game/blecs/systems/ui/ui_game_chunks_info.zig
+++ src/game/blecs/systems/ui/ui_game_chunks_info.zig
@@ -50,7 +50,7 @@ fn showChunkList() !void {
             .flags = .{
                 .resizable = true,
             },
-            .column = 3,
+            .column = 4,
             .outer_size = .{ 2000, 500 },
         },
     )) {
@@ -62,8 +62,10 @@ fn showChunkList() !void {
         });
         zgui.tableSetupColumn("Entity", .{});
         zgui.tableSetupColumn("Visibility", .{});
+        zgui.tableSetupColumn("Toggle", .{});
         zgui.tableHeadersRow();
         var it = game.state.gfx.game_chunks.iterator();
+        var i: usize = 0;
         while (it.next()) |kv| {
             const wp = kv.key_ptr.*;
             const c = kv.value_ptr.*;
@@ -80,9 +82,11 @@ fn showChunkList() !void {
             zgui.text("{d}", .{entity});
             _ = zgui.tableSetColumnIndex(2);
             var is_drawn = false;
+            var can_draw = false;
             const renderer_entity = ecs.get_target(world, entity, entities.block.HasChunkRenderer, 0);
             if (renderer_entity != 0) {
-                if (ecs.has_id(world, renderer_entity, ecs.id(components.gfx.CanDraw))) {
+                can_draw = ecs.has_id(world, renderer_entity, ecs.id(components.gfx.CanDraw));
+                if (can_draw) {
                     is_drawn = true;
                 }
             }
@@ -91,6 +95,20 @@ fn showChunkList() !void {
             } else {
                 zgui.text("hidden", .{});
             }
+            if (zgui.tableSetColumnIndex(3)) {
+                var buffer: [50]u8 = undefined;
+                const btn_label: [:0]u8 = try std.fmt.bufPrintZ(&buffer, "toggle##{d}", .{i});
+                if (zgui.smallButton(btn_label)) {
+                    if (renderer_entity != 0) {
+                        if (can_draw) {
+                            ecs.remove(world, renderer_entity, components.gfx.CanDraw);
+                        } else {
+                            ecs.add(world, renderer_entity, components.gfx.CanDraw);
+                        }
+                    }
+                }
+            }
+            i += 1;
         }
         zgui.endTable();
     }
