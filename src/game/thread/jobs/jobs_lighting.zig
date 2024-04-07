a269045b8385626b4387c7877d0f3b58951c0c41
btipling <bjorn@ambientchill.com>
2024-04-06 20:02:50 -0700

    Adding ambient to things along the side

--- src/game/thread/jobs/jobs_lighting.zig
+++ src/game/thread/jobs/jobs_lighting.zig
@@ -27,23 +27,73 @@ pub const LightingJob = struct {
         });
         const c: *chunk.Chunk = game.state.blocks.game_chunks.get(self.wp) orelse return;
         std.debug.print("got a chunk {}\n", .{c.entity});
-        var z: usize = 0;
+        var z: isize = 0;
         while (z < 64) : (z += 1) {
-            var x: usize = 0;
+            var x: isize = 0;
             while (x < 64) : (x += 1) {
-                var y: usize = 63;
+                var y: isize = 63;
                 const air: u8 = 0;
                 while (true) : (y -= 1) {
-                    const chunk_index = x + y * 64 + z * 64 * 64;
-                    var bd: block.BlockData = block.BlockData.fromId(c.data[chunk_index]);
-                    if (bd.block_id != air) {
-                        bd.setAmbient(.top, .full);
-                        bd.setAmbient(.front, .bright);
-                        bd.setAmbient(.back, .bright);
-                        bd.setAmbient(.left, .bright);
-                        bd.setAmbient(.right, .bright);
-                        c.data[chunk_index] = bd.toId();
-                        break;
+                    // check in 5 directions and mark any block for that surface as lit
+                    {
+                        // front: z+
+                        const chunk_index: usize = @intCast(x + y * 64 + (z + 1) * 64 * 64);
+                        if (chunk_index < chunk.chunkSize) {
+                            var bd: block.BlockData = block.BlockData.fromId(c.data[chunk_index]);
+                            if (bd.block_id != air) {
+                                bd.setAmbient(.front, .full);
+                                c.data[chunk_index] = bd.toId();
+                            }
+                        }
+                    }
+                    {
+                        // back: z-
+                        const ci: isize = x + y * 64 + (z - 1) * 64 * 64;
+                        if (ci >= 0) {
+                            const chunk_index: usize = @intCast(ci);
+                            var bd: block.BlockData = block.BlockData.fromId(c.data[chunk_index]);
+                            if (bd.block_id != air) {
+                                bd.setAmbient(.back, .full);
+                                c.data[chunk_index] = bd.toId();
+                            }
+                        }
+                    }
+                    {
+                        // left: x+
+                        const chunk_index: usize = @intCast((x + 1) + y * 64 + z * 64 * 64);
+                        if (chunk_index < chunk.chunkSize) {
+                            var bd: block.BlockData = block.BlockData.fromId(c.data[chunk_index]);
+                            if (bd.block_id != air) {
+                                bd.setAmbient(.left, .full);
+                                c.data[chunk_index] = bd.toId();
+                            }
+                        }
+                    }
+                    {
+                        // right: x-
+                        const ci: isize = (x - 1) + y * 64 + z * 64 * 64;
+                        if (ci >= 0) {
+                            const chunk_index: usize = @intCast(ci);
+                            var bd: block.BlockData = block.BlockData.fromId(c.data[chunk_index]);
+                            if (bd.block_id != air) {
+                                bd.setAmbient(.right, .full);
+                                c.data[chunk_index] = bd.toId();
+                            }
+                        }
+                    }
+                    {
+                        // check below, if hit, stop checking for this y.
+                        const chunk_index: usize = @intCast(x + y * 64 + z * 64 * 64);
+                        var bd: block.BlockData = block.BlockData.fromId(c.data[chunk_index]);
+                        if (bd.block_id != air) {
+                            bd.setAmbient(.top, .full);
+                            bd.setAmbient(.front, .bright);
+                            bd.setAmbient(.back, .bright);
+                            bd.setAmbient(.left, .bright);
+                            bd.setAmbient(.right, .bright);
+                            c.data[chunk_index] = bd.toId();
+                            break;
+                        }
                     }
                     if (y == 0) break;
                 }
