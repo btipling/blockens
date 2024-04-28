16d34e1240c7b823058d075691eded3929cf67b2
btipling <bjorn@ambientchill.com>
2024-04-27 19:02:33 -0700

    Add a test, bot and top was a stack overflow

--- src/game/block/chunk_compress.zig
+++ src/game/block/chunk_compress.zig
@@ -1,9 +1,10 @@
-current_format_version: u8 = 0,
-data: compressorData = 0,
-top_chunk: [chunk.chunkSize]u64 = undefined,
-bottom_chunk: [chunk.chunkSize]u64 = undefined,
+const current_format_version: u8 = 0;
+
+data: compressorData = .{
+    .format_version = current_format_version,
+},
+chunk_data: [chunk.chunkSize]u64 = undefined,
 allocator: std.mem.Allocator,
-buffer: ?[]u8 = null, // actually holds the value for above
 
 const Compressor = @This();
 const big_chunk_byte_size: usize = chunk.chunkSize * @sizeOf(u64);
@@ -13,46 +14,51 @@ const compressorData = packed struct {
     padding: u56 = 0, // reserved for later and u64 alignment
 };
 
-pub fn init(allocator: std.mem.Allocator, top_chunk: []u64, bottom_chunk: []u64) *Compressor {
-    const c: *Compressor = allocator.create(Compressor);
-    c.* = .{
+pub fn init(allocator: std.mem.Allocator, chunk_bytes: []u64) Compressor {
+    var c: Compressor = .{
         .allocator = allocator,
     };
-    @memcpy(&c.top_chunk, top_chunk);
-    @memcpy(&c.bottom_chunk, bottom_chunk);
-    c.toBytes();
+    @memcpy(&c.chunk_data, chunk_bytes);
     return c;
 }
 
-pub fn deinit(self: Compressor) void {
-    if (self.buffer) |b| self.allocator.free(b);
-    self.allocator.destroy(self);
+fn initFromBytes(allocator: std.mem.Allocator, buffer: []const u8) Compressor {
+    var c: Compressor = .{
+        .allocator = allocator,
+    };
+    const i: usize = @sizeOf(compressorData);
+    const data_bytes: []const u8 = buffer[0..i];
+    const chunk_bytes: []const u8 = buffer[i .. i + big_chunk_byte_size];
+    c.data = std.mem.bytesToValue(compressorData, data_bytes);
+    c.chunk_data = std.mem.bytesToValue([chunk.chunkSize]u64, chunk_bytes);
+    return c;
 }
 
-fn toBytes(self: *Compressor) []const u8 {
+// caller owns bytes
+fn toBytes(self: Compressor) []const u8 {
     const data_bytes: []align(4) const u8 = std.mem.sliceAsBytes(([_]compressorData{self.data})[0..]);
-    const top_chunk_bytes: []align(4) const u8 = std.mem.sliceAsBytes(self.top_chunk);
-    const bottom_chunk_bytes: []align(4) const u8 = std.mem.sliceAsBytes(self.bottom_chunk);
-    if (self.buffer) |b| self.allocator.free(b);
-    self.buffer = std.mem.concat(self.allocator, u8, .{
+    const chunk_bytes: []align(4) const u8 = std.mem.sliceAsBytes(self.chunk_data[0..]);
+    return std.mem.concat(self.allocator, u8, &[_][]const u8{
         data_bytes,
-        top_chunk_bytes,
-        bottom_chunk_bytes,
+        chunk_bytes,
     }) catch @panic("OOM");
 }
 
-fn toData(self: *Compressor, buffer: []const u8) void {
-    if (self.buffer) |b| self.allocator.free(b);
-    self.buffer = self.allocator.dupe(buffer) catch @panic("OOM");
-    var i: usize = @sizeOf(compressorData);
-    const data_bytes: []const u8 = buffer[0..i];
-    const top_chunk_bytes: []const u8 = buffer[i .. i + big_chunk_byte_size];
-    i += big_chunk_byte_size;
-    const bottom_chunk_bytes: []const u8 = buffer[i .. i + big_chunk_byte_size];
-    self.data = std.mem.bytesToValue(compressorData, data_bytes);
-    self.top_chunk = std.mem.bytesToValue([chunk.chunkSize]u64, top_chunk_bytes).*;
-    self.top_chunk = std.mem.bytesToValue([chunk.chunkSize]u64, bottom_chunk_bytes).*;
+test "toBytes and initFromBytes" {
+    const b1_block_id: u9 = 2;
+    const bb1_loc: usize = 101;
+    const bb1: BigBlock.BlockData = BigBlock.BlockData.fromId(b1_block_id);
+    var chunk_data: [chunk.chunkSize]u64 = std.mem.zeroes([chunk.chunkSize]u64);
+    chunk_data[bb1_loc] = bb1.toId();
+    const c1: Compressor = init(std.testing.allocator, chunk_data[0..]);
+    const buffer = c1.toBytes();
+    defer std.testing.allocator.free(buffer);
+    const c2: Compressor = initFromBytes(std.testing.allocator, buffer);
+    const retrieved_bb1: BigBlock.BlockData = BigBlock.BlockData.fromId(c2.chunk_data[bb1_loc]);
+    try std.testing.expectEqual(b1_block_id, retrieved_bb1.block_id);
 }
 
 const std = @import("std");
 const chunk = @import("chunk.zig");
+const block = @import("block.zig");
+const BigBlock = block.big;
