c4096250f6193251537970ec2e4f1f1f2766f11b
btipling <bjorn@ambientchill.com>
2024-05-13 05:39:22 +0200

    improve ui for create world

--- src/game/blecs/systems/ui/ui_create_world_screen.zig
+++ src/game/blecs/systems/ui/ui_create_world_screen.zig
@@ -14,16 +14,47 @@ fn run(it: *ecs.iter_t) callconv(.C) void {
     while (ecs.iter_next(it)) {
         for (0..it.count()) |_| {
             const xPos: f32 = game.state.ui.imguiX(660);
-            const yPos: f32 = game.state.ui.imguiY(300);
+            const yPos: f32 = game.state.ui.imguiY(100);
+            const btn_dms: [2]f32 = game.state.ui.imguiButtonDims();
             zgui.setNextWindowPos(.{ .x = xPos, .y = yPos, .cond = .always });
             zgui.setNextWindowSize(.{
                 .w = game.state.ui.imguiWidth(600),
-                .h = game.state.ui.imguiHeight(300),
+                .h = game.state.ui.imguiHeight(650),
             });
-            if (zgui.begin("Create New World", .{
+            if (zgui.begin("#CreateWorldScreen", .{
                 .flags = zgui.WindowFlags.no_decoration,
             })) {
-                zgui.text("create new world stuff...", .{});
+                const ww = zgui.getWindowWidth();
+                zgui.newLine();
+
+                centerNext(ww);
+                zgui.pushFont(game.state.ui.codeFont);
+                zgui.pushItemWidth(game.state.ui.imguiWidth(200));
+                _ = zgui.inputTextWithHint("Name", .{
+                    .buf = game.state.ui.world_name_buf[0..],
+                    .hint = "world name",
+                });
+
+                centerNext(ww);
+                _ = zgui.inputInt("Seed", .{
+                    .v = &game.state.ui.terrain_gen_seed,
+                });
+                zgui.popItemWidth();
+                zgui.popFont();
+
+                centerNext(ww);
+                var params: helpers.ScriptOptionsParams = .{};
+                if (helpers.scriptOptionsListBox(game.state.ui.terrain_gen_script_options, &params)) |scriptOptionId| {
+                    std.debug.print("selected script: {}\n", .{scriptOptionId});
+                }
+
+                centerNext(ww);
+                if (zgui.button("Create World", .{
+                    .w = btn_dms[0],
+                    .h = btn_dms[1],
+                })) {
+                    // generate world
+                }
             }
             zgui.end();
         }
@@ -31,6 +62,7 @@ fn run(it: *ecs.iter_t) callconv(.C) void {
 }
 
 fn centerNext(ww: f32) void {
+    zgui.newLine();
     zgui.newLine();
     zgui.sameLine(.{
         .offset_from_start_x = ww / 2 - game.state.ui.imguiWidth(125),
@@ -42,6 +74,7 @@ const std = @import("std");
 const ecs = @import("zflecs");
 const zgui = @import("zgui");
 const glfw = @import("zglfw");
+const helpers = @import("ui_helpers.zig");
 const components = @import("../../components/components.zig");
 const screen_helpers = @import("../screen_helpers.zig");
 const game = @import("../../../game.zig");
