5f229b9260028c2ee0709d3ea99f233f8790acdf
btipling <bjorn@ambientchill.com>
2024-05-14 04:23:41 +0200

    saving a world with scripts works

--- src/game/data/sql/v3/world_terrain/create.sql
+++ src/game/data/sql/v3/world_terrain/create.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS world_terrain (
     id INTEGER PRIMARY KEY,
     world_id INTEGER NOT NULL,
-    terrain_gen_script_id INTEGER NOT NULL
+    terrain_gen_script_id INTEGER NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME
 );
